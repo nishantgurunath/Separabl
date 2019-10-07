f1e623e0e15c4de22a7f22ce0435c4e491c3e472
Nishant Gurunath <nishantgurunath@Nishants-MacBook-Air.local>
2019-10-07 04:06:21 -0400

    Files to automate training

---
+++ model.py
@@ -0,0 +1,126 @@
+import torch
+import numpy as np
+import torch.nn as nn
+import torch.nn.utils.rnn as rnn
+
+
+
+
+class multilingual_speech_model(nn.Module):
+
+
+    def __init__(self,num_latent):
+        super(multilingual_speech_model, self).__init__()
+        self.num_latent = num_latent 
+        self.input_size = 321
+        self.ehidden_size = 512
+        self.dinput_size = (num_latent+1)*64
+        self.dhidden_size = 512 
+        self.mult_dim = 2
+        
+        
+        self.encoder = nn.LSTM(self.input_size,self.ehidden_size, num_layers = 2, bidirectional=True) # S x N x 512*2
+        self.fc_gaussian = [(nn.Linear(self.ehidden_size*2, int(self.dinput_size/(num_latent+1))),nn.Linear(self.ehidden_size*2, int(self.dinput_size/(num_latent+1))))]*num_latent
+    
+        self.decoder = nn.LSTM(self.dinput_size, self.dhidden_size, bidirectional = True) # S x N x 512*2
+        if(torch.cuda.is_available()):
+            self.c = nn.Parameter(torch.zeros(2,self.dhidden_size).type(torch.cuda.FloatTensor))
+            self.h = nn.Parameter(torch.zeros(2,self.dhidden_size).type(torch.cuda.FloatTensor))
+        else:
+            self.c = nn.Parameter(torch.zeros(2,self.dhidden_size))
+            self.h = nn.Parameter(torch.zeros(2,self.dhidden_size))
+
+        self.context = nn.Linear(self.dhidden_size*2, int(self.dinput_size/(num_latent+1))) # S x N x 64
+
+        self.feature = nn.Linear(self.dhidden_size*2, self.dhidden_size*4) # S x N x 2048
+        self.activation = nn.Tanh()
+        self.scoring = nn.Linear(self.dhidden_size*4, self.input_size) # S x N x 321
+   
+    def encoder_block(self,inp,eps=0,eta=1,test=0): 
+        
+        x = rnn.pack_sequence(inp) 
+        x,(h,c) = self.encoder(x)
+        x,l = rnn.pad_packed_sequence(x) # S x N x 512*2
+        S,N,E = x.shape  
+
+        # Mask
+        if torch.cuda.is_available():
+            mask = torch.zeros((N,S)).type(torch.cuda.FloatTensor) # N x S
+        else:
+            mask = torch.zeros((N,S)).type(torch.FloatTensor) # N x S
+
+        for i in range(len(l)):
+            mask[i,0:l[i]] = torch.ones(l[i]) 
+        mask = torch.transpose(mask,0,1) # S x N
+
+
+        z = torch.zeros((self.num_latent,S,N,self.dinput_size//(self.num_latent+1)))
+        mu = torch.zeros((self.num_latent,S,N,self.dinput_size//(self.num_latent+1)))
+        log_var = torch.zeros((self.num_latent,S,N,self.dinput_size//(self.num_latent+1)))
+        for i,fc in enumerate(self.fc_gaussian):
+            mu_temp = fc[0](x)
+            log_var_temp = fc[1](x)
+            std_temp = torch.exp(0.5*log_var_temp)
+            if torch.cuda.is_available():
+                z_temp = (mu_temp+eps) + eta*std_temp*(torch.randn(std_temp.shape).type(torch.cuda.FloatTensor))
+            else:
+                z_temp = (mu_temp+eps) + eta*std_temp*(torch.randn(std_temp.shape).type(torch.FloatTensor))
+
+            z[i] = z_temp 
+            mu[i] = mu_temp 
+            log_var[i] = log_var_temp
+
+
+        return z,mu,log_var,mask
+
+
+    def decoder_block(self,z):
+        K,S,N,E = z.shape
+        c = self.h.view(-1).unsqueeze(0) # 1 x 512*2
+        c = c.expand(N,-1) # N x 512*2
+        c = self.context(c) # N x 64
+        inp = z.permute(1,2,0,3)[0].contiguous().view(N,K*E)
+        inp = torch.cat((inp,c),dim=1).unsqueeze(0)
+        h_0 = self.h.unsqueeze(1) # 2 x 1 x 512
+        h_0 = h_0.expand(-1,N,-1).contiguous() # 2 x N x 512
+        c_0 = self.c.unsqueeze(1) # 2 x 1 x 512
+        c_0 = c_0.expand(-1,N,-1).contiguous() # 2 x N x 512
+       
+        x_n,(h_n,c_n) = self.decoder(inp,(h_0,c_0)) 
+
+        out = x_n
+
+        for i in range(1,S): 
+            c = x_n.view(N,-1) # N x 512*2
+            c = self.context(c) # N x 64
+            inp = z.permute(1,2,0,3)[i].contiguous().view(N,K*E)
+            inp = torch.cat((inp,c),dim=1).unsqueeze(0)
+            x_n,(h_n,c_n) = self.decoder(inp,(h_n,c_n))
+            out = torch.cat((out,x_n), dim=0)
+
+
+        return out
+
+
+
+    def forward(self,inp,eps=0,eta=1,test=0):
+        S = len(inp[0])
+        N = len(inp)
+        z,mu,log_var,mask = self.encoder_block(inp,eps,eta,test)
+
+        out = self.decoder_block(z)
+        out = self.feature(out)
+        out = self.activation(out)
+        out = self.scoring(out)
+
+        return out,mu,log_var,mask
+    
+
+if __name__ == "__main__":
+    x = torch.zeros((300,321))
+    y = torch.ones((300,321))
+    z = torch.ones((300,321))*2
+    x = [x,y,z]
+    model = multilingual_speech_model(5)
+    print (torch.cuda.is_available())
+    print (model(x)[0].shape) 
---
+++ run_train.py
@@ -0,0 +1,218 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+import torch.optim as optim
+import torch.nn.utils.rnn as rnn
+from sklearn.mixture import GaussianMixture
+import argparse
+from model import multilingual_speech_model
+
+
+## Parameters
+# Data - 3 sec, 321x300, sr=16000, 100 frames/sec 
+
+## Arguments - No. of latent variables or GMM likelihood, Train Data Path
+
+## Model
+
+## Train
+
+## Save Best Model
+
+
+def kl_anneal_function(step, k=0.0050, x0=4000, anneal_function='logistic'):
+    if anneal_function == 'logistic':
+        return float(1/(1+np.exp(-k*(step-x0))))
+    elif anneal_function == 'linear':
+        return min(1, step/x0)
+
+## Dataloader Class
+class SpeechModelDataLoader(DataLoader):
+    """
+        TODO: Define data loader logic here
+    """
+    def __init__(self, data, batch_size, shuffle=True):
+        self.data = data
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+
+    def __iter__(self):
+        # concatenate your articles and build into batches
+        data = self.data
+        index = np.arange(len(data))
+        if(self.shuffle==True):
+            np.random.shuffle(index)
+        data = data[index]
+        num_batches = len(data)//self.batch_size
+        print (num_batches)
+        num_res = len(data)%self.batch_size
+        num_utt = num_batches*self.batch_size
+
+        i = 0
+        while(i<num_batches):
+            out_data = []
+            s = i*self.batch_size
+            e = (i+1)*self.batch_size
+            index1 = np.arange(s,e)
+            lengths = [len(data[j]) for j in range(s,e)] 
+            min_len = min(min(lengths)-1,300)
+
+            for j in range(s,e):
+                k = np.random.randint(0,len(data[j])-min_len)
+                if torch.cuda.is_available():
+                    out_data.append(torch.cuda.FloatTensor(data[j][k:k+min_len]))
+                else:
+                    out_data.append(torch.FloatTensor(data[j][k:k+min_len]))
+            yield out_data
+            i = i + 1
+            
+
+        ## Residual Data
+        if(num_res > 0):
+            out_data = []
+            s = num_utt
+            e = len(data)
+            index1 = np.arange(s,e)
+            lengths = [len(data[j]) for j in range(s,e)] 
+            min_len = min(min(lengths)-1,300)
+
+            for j in range(s,e):
+                k = np.random.randint(0,len(data[j])-min_len)
+                if torch.cuda.is_available():
+                    out_data.append(torch.cuda.FloatTensor(data[j][k:k+min_len]))
+                else:
+                    out_data.append(torch.FloatTensor(data[j][k:k+min_len]))
+            yield out_data
+
+
+
+
+class model_run:
+
+    def train(self, num_epochs, num_latent, data):
+
+        ## Parameters ##
+        batch_size = 16
+        lr = 0.001
+        print (len(data))
+
+        ## Model ##
+        model = multilingual_speech_model(num_latent)
+        model = model.cuda() if torch.cuda.is_available() else model
+        #model_state = torch.load('models_dc/model49.pt')
+        #model.load_state_dict(model_state)    
+
+
+        ##Data Loader   ## 
+        loader = SpeechModelDataLoader(abs(data), shuffle=True, batch_size=batch_size)
+
+
+        # Criterion - Negative Log Likelihood + KL Divergence Z
+        criterion_mse = nn.MSELoss(reduction = 'none')
+               
+        # Optimizer
+        optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=0.001)
+        updates = 0
+        for e in range(num_epochs):
+            epoch_loss = 0
+            epoch_KL = 0
+            epoch_MSE = 0
+            model.train()
+            i = 0
+            samples = 0
+            for data in loader:
+                optimizer.zero_grad()
+                padded_data = rnn.pad_sequence(data) 
+                S,N,E = padded_data.shape
+                
+                out,mu,log_var,mask = model(data)
+                
+                samples += N
+                updates += 1
+
+                out = out.view(-1,E)*mask.contiguous().view(-1,1)
+                #mu_s = mu_s.view(-1,mu_s.shape[2])*mask.contiguous().view(-1,1)
+                #log_var_s = log_var_s.view(-1,log_var_s.shape[2])*mask.contiguous().view(-1,1)
+                #mu_r = mu_r.view(-1,mu_r.shape[2])*mask.contiguous().view(-1,1)
+                #log_var_r = log_var_r.view(-1,log_var_r.shape[2])*mask.contiguous().view(-1,1)
+                #mu_r1 = mu_r1.view(-1,mu_r1.shape[2])*mask.contiguous().view(-1,1)
+                #log_var_r1 = log_var_r1.view(-1,log_var_r1.shape[2])*mask.contiguous().view(-1,1)
+                #mu_r2 = mu_r2.view(-1,mu_r2.shape[2])*mask.contiguous().view(-1,1)
+                #log_var_r2 = log_var_r2.view(-1,log_var_r2.shape[2])*mask.contiguous().view(-1,1)
+
+                K,S,N,E1 = mu.shape
+                mu = mu.permute(1,2,0,3).contiguous().view(-1,K,E1)*mask.contiguous().view(-1,1,1)
+                mu = mu.permute(1,0,2)
+                log_var = log_var.permute(1,2,0,3).contiguous().view(-1,K,E1)*mask.contiguous().view(-1,1,1)
+                log_var = log_var.permute(1,0,2)
+
+                KL,MSE = self.loss_fn(out,padded_data.view(-1,E),mu,log_var,criterion_mse)
+                MSE = MSE/(S) 
+                weight =  kl_anneal_function(updates,k=40*batch_size/(len(data)*num_epochs),x0=len(data)*num_epochs//(batch_size*2))
+                NLL =  MSE + (torch.sum(KL))*weight
+                NLL.backward()
+                epoch_loss += NLL.item()
+                epoch_KL += torch.sum(KL).item()/K
+                epoch_MSE += MSE.item()
+                #torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+                optimizer.step()
+                i = i + 1
+                if(i%10==0):
+                    print("Epoch: ", e, "Iter: ", i, "Loss: ", (epoch_loss/(samples)), "KL", epoch_KL/samples,  "MSE ", epoch_MSE/samples)
+
+           
+            #if (e+1) % 1 == 0:
+            #    torch.save(model.state_dict(), "models_dc/model" + str(e) + ".pt")
+            #    print("Epoch: ", e, "Iter: ", i, "Loss: ", (epoch_loss/(samples)))
+
+            if epoch_KL/samples < 60 and epoch_MSE/samples < 250:
+                torch.save(model.state_dict(), "models/model" + str(e) + ".pt")
+                print("Epoch: ", e,  "Loss: ", (epoch_loss/(samples)), "KL", epoch_KL/samples,  "MSE ", epoch_MSE/samples)
+                #break 
+
+
+    def loss_fn(self,recon_x, x, mu, log_var, criterion_mse):
+        MSE = criterion_mse(recon_x, x)
+        MSE = torch.sum(MSE)
+        # see Appendix B from VAE paper:
+        # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
+        # https://arxiv.org/abs/1312.6114
+        # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+
+        K,N,E = mu.shape
+        mu = mu.contiguous().view(K,-1)
+        log_var = log_var.contiguous().view(K,-1)
+        KLD = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp(), dim = 1)
+        
+        return KLD, MSE
+
+
+
+if __name__ == "__main__":
+
+    ## Parse Arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-p", "--path", required=True, help="Path to Train Data")
+    args = vars(ap.parse_args())
+
+    ## Load Data
+    #train_data = np.load(args['path'])
+    train_data = np.array([np.zeros((300,321))]*32)
+    num_epochs = 2
+
+    ## Find No. of latent nodes
+    K = 3
+    #while(1):
+    #    gmm = GaussianMixture(n_components=K)
+    #    gmm.fit(train_data)
+    #    likelihood = gmm.lower_bound_
+    #    if(likelihood > 1070):
+    #        break
+    #    K += 1
+
+
+    ## Train
+    run = model_run()
+    run.train(num_epochs, K-1, train_data)
+
